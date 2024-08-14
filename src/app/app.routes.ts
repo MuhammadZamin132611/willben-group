f67264e667870b5d329aa9b4e77e52424874f7e3
Zamin <Muhammad.Zamin@pratian.com>
2024-08-14 15:57:18 +0530

    contact-component-added was done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home/home.component';
+import { ContactComponent } from './modules/contact/contact.component';
 
 export const routes: Routes = [
     {path:'',component:HomeComponent},
+    {path:'contact',component:ContactComponent},
     {path:'**',component:HomeComponent},
 ];
