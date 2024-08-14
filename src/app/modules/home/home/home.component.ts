f9eaddb00df596154a5fc93d9337faf008a86b9f
Zamin <Muhammad.Zamin@pratian.com>
2024-08-14 15:29:59 +0530

    our-clients-say was done

--- src/app/modules/home/home/home.component.ts
+++ src/app/modules/home/home/home.component.ts
@@ -3,11 +3,12 @@ import { WhoAreWeComponent } from "./who-are-we/who-are-we.component";
 import { StartFastComponent } from './start-fast/start-fast.component';
 import { ServicesWeOfferComponent } from "./services-we-offer/services-we-offer.component";
 import { OurImpactComponent } from "./our-impact/our-impact.component";
+import { OurClientsSayComponent } from "./our-clients-say/our-clients-say.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [WhoAreWeComponent, StartFastComponent, ServicesWeOfferComponent, OurImpactComponent],
+  imports: [WhoAreWeComponent, StartFastComponent, ServicesWeOfferComponent, OurImpactComponent, OurClientsSayComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/modules/home/home/our-clients-say/our-clients-say.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-clients-say',
+  standalone: true,
+  imports: [],
+  templateUrl: './our-clients-say.component.html',
+  styleUrl: './our-clients-say.component.scss'
+})
+export class OurClientsSayComponent {
+  topNotchDesc: string = `Through Willben Group Private Limited, I've participated in multiple outsourcing projects for North American clients, with their performance consistently exceeding that of in-house teams. Their remarkable productivity sets the company apart, among other advantages.`;
+  incredibleDesc: string = `I've engaged in numerous outsourcing projects for North American clients through Willben Group Private Limited. Their performance consistently surpasses that of clients' in-house teams, exemplifying remarkable productivity. This advantage, among others, sets the company apart significantly.`;
+  phenominalDesc: string = `We are highly pleased with Willben Group Private Limited' performance as our contractor. Their team is extremely responsive, possesses a deep grasp of the subject matter, and consistently delivers code at an impressively rapid pace. This swiftness in code delivery holds particular significance for our travel-oriented business.`;
+  extraodinaryDesc: string = `Working with you has been a terrific experience. Yes, your customer assistance is excellent, and your SEO and SMO services are really effective. I'm confident that I won't ever collaborate with anyone else on my digital marketing campaign.`
+}
