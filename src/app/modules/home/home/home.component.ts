37fb3ec5c4dff5b8386577fe902734ab7dcb9f8f
Zamin <Muhammad.Zamin@pratian.com>
2024-08-14 13:43:17 +0530

    our impact was done

--- src/app/modules/home/home/home.component.ts
+++ src/app/modules/home/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { WhoAreWeComponent } from "./who-are-we/who-are-we.component";
 import { StartFastComponent } from './start-fast/start-fast.component';
 import { ServicesWeOfferComponent } from "./services-we-offer/services-we-offer.component";
+import { OurImpactComponent } from "./our-impact/our-impact.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [WhoAreWeComponent, StartFastComponent, ServicesWeOfferComponent],
+  imports: [WhoAreWeComponent, StartFastComponent, ServicesWeOfferComponent, OurImpactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/modules/home/home/our-impact/our-impact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-impact',
+  standalone: true,
+  imports: [],
+  templateUrl: './our-impact.component.html',
+  styleUrl: './our-impact.component.scss'
+})
+export class OurImpactComponent {
+
+}
