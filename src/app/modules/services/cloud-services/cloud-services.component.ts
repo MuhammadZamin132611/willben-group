55516e4965d397bcf239c58049db68580afdffea
Zamin <Muhammad.Zamin@pratian.com>
2024-08-18 12:28:48 +0530

    types of cloud services dynamically added

--- src/app/modules/services/cloud-services/cloud-services.component.ts
+++ src/app/modules/services/cloud-services/cloud-services.component.ts
@@ -3,11 +3,12 @@ import { ServicesTopPartComponent } from '../subComponent/services-top-part/serv
 import { SingleCardServiceComponent } from '../subComponent/single-card-service/single-card-service.component';
 import { LeadingMultinationalClientsComponent } from "../subComponent/leading-multinational-clients/leading-multinational-clients.component";
 import { MaterialModule } from '../../../shared/material.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cloud-services',
   standalone: true,
-  imports: [MaterialModule, ServicesTopPartComponent, SingleCardServiceComponent, LeadingMultinationalClientsComponent],
+  imports: [CommonModule, MaterialModule, ServicesTopPartComponent, SingleCardServiceComponent, LeadingMultinationalClientsComponent],
   templateUrl: './cloud-services.component.html',
   styleUrl: './cloud-services.component.scss'
 })
@@ -83,4 +84,101 @@ export class CloudServicesComponent {
   heading: string = `For us, Cloud Platforms Digital Capabilities`;
   paragraph1: string = `Willben Group Private Limited offers cloud application development services across all major cloud platforms. Our group of specialists in solution design selects the suitable Cloud platform.`;
 
+  cloudPlatform: CloudPlatform = {
+    singleCardDetails: [
+      {
+        image: "../../../../assets/awsImages.svg",
+        imageName: "aws",
+        paragraphs: [
+          {
+            paragraph: "AWS Consulting Services"
+          },
+          {
+            paragraph: "We have been providing enterprise-level AWS-based solutions and a wide range of AWS consulting services as a certified Amazon Consulting Partner. We are able to provide clients throughout the world with value-added solutions because of Amazon products' special capabilities."
+          },
+          {
+            paragraph: "We provide to build your cloud-native development or AWS migration plan and make it possible for your AWS-based infrastructure to be used economically."
+          },
+          {
+            paragraph: "Our offerings include:"
+          }
+        ],
+        allLists: [
+          {
+            list: "Services for AWS migration"
+          },
+          {
+            list: "Development services with AWS"
+          },
+          {
+            list: "Built on AWS microservices"
+          },
+          {
+            list: "Architecture without servers"
+          }
+        ]
+      },
+      {
+        image: "../../../../assets/googleCloud.svg",
+        imageName: "googleCloud",
+        paragraphs: [
+          {
+            paragraph: "Google Cloud Consulting Services"
+          },
+          {
+            paragraph: "We provide full-service Google Cloud consultancy to assist businesses of all sizes in maximizing the benefits of the distinctive capabilities provided by Google Cloud Platform (GCP)."
+          },
+          {
+            paragraph: "We offer round-the-clock Google Cloud consulting and support services, and our Google Cloud consulting professionals will help you effectively design and create a Google Cloud infrastructure."
+          },
+        ],
+      },
+      {
+        image: "../../../../assets/microSoftAzure.svg",
+        imageName: "microSoftAzure",
+        paragraphs: [
+          {
+            paragraph: "Microsoft Azure Consulting Services"
+          },
+          {
+            paragraph: "We offer round-the-clock Google Cloud consulting and support services in addition to helping you effectively plan and construct a Google Cloud infrastructure with the help of our Google Cloud consulting professionals."
+          },
+          {
+            paragraph: "Our offerings include:"
+          }
+        ],
+        allLists: [
+          {
+            list: "migration services for Azure"
+          },
+          {
+            list: "services for developing on Azure"
+          },
+          {
+            list: "Services for optimizing Azure environments"
+          },
+        ]
+      },
+    ]
+  }
+
+}
+
+interface CloudPlatform {
+  singleCardDetails: CardDetail[];
+}
+
+interface CardDetail {
+  image: string;
+  imageName: string;
+  paragraphs: Paragraph[];
+  allLists?: ListItem[];
+}
+
+interface Paragraph {
+  paragraph: string;
+}
+
+interface ListItem {
+  list: string;
 }
