1daaed46b578f552abc6253e9479bbbaebd0d9ee
Zamin <Muhammad.Zamin@pratian.com>
2024-09-22 20:05:18 +0530

    show data dynamically

--- src/app/modules/technologies/react/react.component.ts
+++ src/app/modules/technologies/react/react.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { HeadingDescriptionComponent } from "../sub-component/heading-description/heading-description.component";
 import { ExplainPlatformComponent } from "../sub-component/explain-platform/explain-platform.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-react',
   standalone: true,
-  imports: [HeadingDescriptionComponent, ExplainPlatformComponent],
+  imports: [HeadingDescriptionComponent, ExplainPlatformComponent, RouterLink],
   templateUrl: './react.component.html',
   styleUrl: './react.component.scss'
 })
 export class ReactComponent {
+
+  explainPlatform = {
+    image: `../../../../../assets/happyTarget.png`,
+    heding: `Flawless Performance.Great User Experience`,
+    paragraph: `From start-ups to well-established companies, almost everyone these days uses ReactJS to build native apps. This open-source JavaScript framework specializes in building UIs and has been designed to help create apps for both Android and iOS platforms. ReactJS is considered one of the best front-end development frameworks as it enables developers to build robust & complex products. Utilize our React outsourcing capacity and build a robust solution for your own business!`,
+    aboutTechnologies: [
+      {
+        count: '250+',
+        aboutHeading: `Skilled Engineers`,
+        aboutParagraph: `Whether you hire one or 10 React developers, we guarantee that you will be working only with Middle and Senior level engineers.`
+      },
+      {
+        count: '275+',
+        aboutHeading: `React Projects`,
+        aboutParagraph: `With years of experience delivering top-notch React development services, our skilled developers can handle projects of any complexity.`
+      },
+      {
+        count: '8+',
+        aboutHeading: `Years of Experiences`,
+        aboutParagraph: `Our most experienced React engineers were early adopters of the library and started implementing its features as early as 2013.`
+      },
+    ]
+  }
+  
 
 }
--- src/app/modules/technologies/sub-component/explain-platform/explain-platform.component.ts
+++ src/app/modules/technologies/sub-component/explain-platform/explain-platform.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-explain-platform',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './explain-platform.component.html',
   styleUrl: './explain-platform.component.scss'
 })
 export class ExplainPlatformComponent {
 
+  @Input() explainPlatform: Platform = {
+    image: ``,
+    heding: ``,
+    paragraph: ``,
+    aboutTechnologies: [
+      {
+        count: '',
+        aboutHeading: ``,
+        aboutParagraph: ``
+      },
+    ]
+  }
+
+}
+
+interface Platform {
+  image: string;
+  heding: string;
+  paragraph: string;
+  aboutTechnologies: Card[]
+}
+
+interface Card {
+  count: string;
+  aboutHeading: string;
+  aboutParagraph: string;
 }
