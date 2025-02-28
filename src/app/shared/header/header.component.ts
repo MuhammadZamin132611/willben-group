7bf7168941eee0c570f8746635ce2ea5a9454494
Zamin <Muhammad.Zamin@pratian.com>
2025-02-28 13:26:15 +0530

    add menu design

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../material.module';
 import { RouterLink } from '@angular/router';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MaterialModule, RouterLink],
+  imports: [MaterialModule, RouterLink, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  isPopupVisibleLanguage = false;
+  togglePopupLanguage() {
+    this.isPopupVisibleLanguage = !this.isPopupVisibleLanguage
+  }
 }
+
