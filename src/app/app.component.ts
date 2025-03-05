e46b9fa0febb95aab2e8744a87bb30d710557ddc
Zamin <Muhammad.Zamin@pratian.com>
2025-03-05 15:47:00 +0530

    update build and app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { MaterialModule } from './shared/material.module';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
 import { ScreenOrientation } from '@capacitor/screen-orientation';
+import { StatusBar, Style } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +13,36 @@ import { ScreenOrientation } from '@capacitor/screen-orientation';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'willben-group';
 
-  constructor(){
+  constructor(private router: Router){
     ScreenOrientation.lock({ orientation: 'portrait' });
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
+    });
+  }
+
+  private setStatusBarColor() {
+    const body = document.getElementsByTagName('body')[0];
+    const observer = new MutationObserver(() => {
+      if (body.classList.contains('ion-color-light')) {
+        StatusBar.setStyle({ style: Style.Dark });
+      } else {
+        StatusBar.setStyle({ style: Style.Light });
+      }
+    });
+
+    observer.observe(body, {
+      attributes: true,
+      attributeFilter: ['class'],
+      childList: false,
+      characterData: false,
+    });
   }
 }
