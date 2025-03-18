2937e06bcdc181f0ffb5fcb720d85d1cef03d482
Zamin <Muhammad.Zamin@pratian.com>
2025-03-18 17:17:03 +0530

    update app routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { MaterialModule } from './shared/material.module';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
-import { ScreenOrientation } from '@capacitor/screen-orientation';
-import { StatusBar, Style } from '@capacitor/status-bar';
+import { App as CapacitorApp } from '@capacitor/app';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -16,33 +16,24 @@ import { StatusBar, Style } from '@capacitor/status-bar';
 export class AppComponent implements OnInit {
   title = 'willben-group';
 
-  constructor(private router: Router){
-    // ScreenOrientation.lock({ orientation: 'portrait' });
+  constructor(private router: Router, private location: Location) {
+  }
+
+  handleBackButton() {
+    CapacitorApp.addListener('backButton', ({ canGoBack }) => {
+      if (this.router.url !== '/') {
+        // Go back using Angular routing
+        this.location.back();
+      } else {
+        // Optionally exit app only from home
+        CapacitorApp.exitApp();
+      }
+    });
   }
 
   ngOnInit(): void {
-    // this.router.events.subscribe(event => {
-    //   if (event instanceof NavigationEnd) {
-    //     window.scrollTo(0, 0);
-    //   }
-    // });
+
   }
 
-  // private setStatusBarColor() {
-  //   const body = document.getElementsByTagName('body')[0];
-  //   const observer = new MutationObserver(() => {
-  //     if (body.classList.contains('ion-color-light')) {
-  //       StatusBar.setStyle({ style: Style.Dark });
-  //     } else {
-  //       StatusBar.setStyle({ style: Style.Light });
-  //     }
-  //   });
 
-  //   observer.observe(body, {
-  //     attributes: true,
-  //     attributeFilter: ['class'],
-  //     childList: false,
-  //     characterData: false,
-  //   });
-  // }
 }
